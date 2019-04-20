3b66888f7df457ff8c1c624978e2ccbd6d3250c9
Adhil Juvane <adhilmp3@gmail.com>
2019-04-20 11:27:07 +0530

    src changed

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
-import Login from "./Components/Login";
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() { }
+  componentDidMount() {}
 
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/" component={Login} />
+          <Route path="/" component={Home} />
         </Switch>
       </BrowserRouter>
     );
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,150 +1,107 @@
 import React, { Component } from "react";
-import { Icon, Table, Empty, Button } from "antd";
+import { message } from "antd";
 import "./index.css";
-import { db, Auth } from "../../config";
-import { auth } from "firebase";
+import axios from "axios";
+
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicmFiaXJvc2hhbiIsImEiOiJjanVrNXg4aTcwbzFjNDNucTE3YThrY3QzIn0.SAB8o_a-l9FuLprNGARBAA";
 
 export default class index extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: "",
-      data: [],
-      allUrls: [],
-    }
+      position: { lng: 76.3282, lat: 10.0443 },// Default CUSAT
+      another: { lng: 76.32908075445818, lat: 10.04501723322933 }
+    };
   }
 
   componentDidMount() {
-    this.getActiveData();
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v9",
+      maxBounds: new mapboxgl.LngLatBounds(
+        [76.29055, 10.007523],
+        [76.383933, 10.083249]
+      ),
+      center: [76.3282, 10.0443],
+      zoom: 14
+    });
+
+    var geojson = {
+      type: 'FeatureCollection',
+      features: [{
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [76.3282, 10.0443]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [76.32933, 10.045]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }]
+    };
+
+    var that = this;
+    geojson.features.forEach(function (marker) {
+      // create a HTML element for each feature
+      var el = document.createElement('div');
+      el.className = 'marker';
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(that.map);
+    });
+    this.getDirections();
+
   }
 
-  getActiveData = () => {
-    Auth.onAuthStateChanged((user) => {
-      if (user) {
-        const list = [];
-        var that = this;
-        db.ref('store')
-          .child('orders')
-          .child('active')
-          .on("value", (data) => {
-            data.forEach((child) => {
-              var item = {
-                user: child.val().user,
-                description: child.val().description,
-                mobile: child.val().mobile,
-                status: child.val().status,
-                urls: child.val().urls,
-                timestamp: child.val().timestamp,
-                key: child.key
-              }
-
-              list.push(item);
-              that.setState({ data: list })
-            })
-          })
-      } else {
-        this.props.history.push("/");
-      }
+  getDirections = () => {
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/76.3282,10.0443;76.32933,10.045.geometries=geojson&access_token=` + mapboxgl.accessToken, {
+      accessToken: "pk.eyJ1IjoicmFiaXJvc2hhbiIsImEiOiJjanVrNXg4aTcwbzFjNDNucTE3YThrY3QzIn0.SAB8o_a-l9FuLprNGARBAA"
     })
-  }
-
-  expandedRowRender = (data, i) => {
-    return (
-      <div key={i} className="oneRow" style={{ display: "flex", flexDirection: "column" }}>
-        <div style={{ margin: "2vw 0vw" }}>{i}</div>
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
-          <div>Type</div>
-          <div style={{ marginLeft: "4vw" }}>{data.type}</div>
-        </div>
-        <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
-          <div>URL</div>
-          <div style={{ marginLeft: "4vw" }}><a style={{ color: "black" }} href={data.urls} >Download File</a></div>
-        </div>
-      </div>
-    )
-  }
-
-  onAcceptOrder = (record) => {
-    db.ref('store')
-      .child('orders')
-      .child('active')
-      .child(record.key).set({
-        description: record.description,
-        mobile: record.mobile,
-        status: "processing",
-        timestamp: record.timestamp,
-        urls: record.urls,
-        user: record.user
+      .then(res => res.json())
+      .then(route => {
+        console.log("route", route)
       })
-    this.getActiveData();
   }
 
-  onDone = (record) => {
-    db.ref('store')
-      .child('orders')
-      .child('done')
-      .child(record.key).set({
-        description: record.description,
-        mobile: record.mobile,
-        status: "done",
-        timestamp: record.timestamp,
-        urls: record.urls,
-        user: record.user
-      })
+  checkIfWithinBounds(x, y) {
+    const a = 76.29055;
+    const b = 10.007523;
+    const c = 76.383933;
+    const d = 10.083249;
 
-    db.ref('store')
-      .child('orders')
-      .child('active')
-      .child(record.key).remove();
-    this.getActiveData();
+    if (x > a && x < c && y > b && y < d) return true;
+    else return false;
   }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
 
   render() {
+    const style = {
+      position: "absolute",
+      top: "4%",
+      bottom: 0,
+      width: "100%"
+    };
 
-    const columns = [{
-      title: 'User',
-      dataIndex: 'user',
-      key: 'user',
-    }, {
-      title: 'Mobile',
-      dataIndex: 'mobile',
-      key: 'mobile',
-    }, {
-      title: 'Description',
-      dataIndex: 'description',
-      key: 'description',
-    }, {
-      title: 'Time Stamp',
-      dataIndex: 'timestamp',
-      key: 'timestamp',
-    }, {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-    }, {
-      title: 'Action',
-      key: 'action',
-      render: (record) => {
-        return (
-          record.status === "active" ?
-            <Button onClick={() => { this.onAcceptOrder(record) }}>Accept Order</Button> :
-            <Button onClick={() => { this.onDone(record) }}>Done</Button>
-        )
-      }
-    }];
-
-    return (
-      <div className="homeMainDiv">
-        <h1 style={{ display: "flex", alignItems: "center", justifyContent: "center", fontWeight: "900" }}>Home Page.</h1>
-        <Table
-          dataSource={this.state.data}
-          pagination={false}
-          expandedRowRender={record => (
-            <div>{record.urls ? record.urls.map(this.expandedRowRender, this) : <Empty />}</div>
-          )}
-          columns={columns}
-        />
-      </div>
-    );
+    return <div style={style} ref={el => (this.mapContainer = el)} />;
   }
 }
